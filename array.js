7f27a07885faf3bfcaa1f20bbe7f34dd7b04b64f
Jackson0Souza <129568316+Jackson0Souza@users.noreply.github.com>
2024-06-11 19:12:02 +0000

    arrays

---
+++ array.js
@@ -0,0 +1,24 @@
+// const nomes = ['bruno', 'joão', 'maria'] //array
+// nomes[3] = "paulo" // adiciona um novo valor no array no indice informado 
+// nomes[2] = "2" // atualizar o valor no array no indice informado
+// console.log(nomes(2))
+// console.log(nomes.length) // atraves do length é um atributo do array que informa o tamanho  dele.
+// nomes.push('kaique')
+// console.log(nomes.length);
+
+const comidasFavoritas = []
+//salgados
+comidasFavoritas[0] = 'coxinha'
+comidasFavoritas[1] = 'pizza'
+comidasFavoritas[2] = 'pastel'
+//frutas
+comidasFavoritas[3] = 'acerola'
+comidasFavoritas[4] = 'laranja'
+comidasFavoritas[5] = 'abacate'
+
+function ordemAlfabetica(comidas) {
+
+    const comidasOrdenadas = []
+    
+}
+
